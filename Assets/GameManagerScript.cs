2dd8e7ab375dd47126a778fb5318453db1701aab
KeroppiKun <fuga0809ace@gmail.com>
2023-04-25 16:54:30 +0900

    Wallのマテリアル変更

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -67,11 +67,11 @@ public class GameManagerScript : MonoBehaviour
     {
         map = new int[,]{
             { 3,3,3,3,3,3,3,3,3,3 },
-            { 3,0,0,0,0,0,0,0,0,3 },
-            { 3,0,0,0,0,0,0,2,0,3 },
-            { 3,0,0,0,1,0,0,0,0,3 },
-            { 3,0,2,0,0,0,0,0,0,3 },
-            { 3,0,0,0,0,0,0,0,0,3 },
+            { 3,0,0,0,0,3,0,0,0,3 },
+            { 3,0,0,0,0,3,0,2,3,3 },
+            { 3,0,0,0,1,3,3,0,0,3 },
+            { 3,0,0,3,2,0,0,0,0,3 },
+            { 3,0,0,3,0,0,0,0,0,3 },
             { 3,3,3,3,3,3,3,3,3,3 }
         };
 
@@ -104,7 +104,6 @@ public class GameManagerScript : MonoBehaviour
                     wallPrefab,
                     new Vector3(x, map.GetLength(0) - y, 0),
                     Quaternion.identity);
-                    field[y,x].transform.GetComponent<MeshRenderer>().material.color = Color.red;
                 }
 
             }
